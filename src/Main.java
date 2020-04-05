db7a65957a17f29116cca66707f85cd6d306f00d
Yusuf <bsse1013@iit.du.ac.bd>
2020-04-05 15:39:16 +0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        String baseUrl = "https://dictionary.cambridge.org/dictionary/english/";
+        File file = new File("small.txt");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scanner.hasNext())
+        {
+            String word = scanner.next(), url = baseUrl + word;
+            Document document = null;
+            try {
+                document = Jsoup.connect(url).get();
+                System.out.println(document.title());
+                //Elements paragraphs = document.getElementsByTag("div");
+                Elements paragraphs = document.getElementsByClass("ddef_h");
+                for (Element paragraph : paragraphs) {
+                    System.out.println(paragraph.text());
+                }
+                System.out.println(url);
+            } catch (IOException e) {
+                System.out.println("ERROR: " + url + " does not exist");
+            }
+
+        }
+
+
+        /*Document document = Jsoup.connect(url).get();
+        System.out.println(document.title());
+        //Elements paragraphs = document.getElementsByTag("div");
+        Elements paragraphs = document.getElementsByClass("ddef_h");
+        for (Element paragraph : paragraphs) {
+            System.out.println(paragraph.text());
+        }*/
+    }
+}
---
+++ src/TEmp.java
@@ -0,0 +1,110 @@
+public class TEmp {
+    public static void main(String[] args) {
+        String shit = "a\n" +
+                "about\n" +
+                "all\n" +
+                "also\n" +
+                "and\n" +
+                "as\n" +
+                "at\n" +
+                "be\n" +
+                "because\n" +
+                "but\n" +
+                "by\n" +
+                "can\n" +
+                "come\n" +
+                "could\n" +
+                "day\n" +
+                "do\n" +
+                "even\n" +
+                "find\n" +
+                "first\n" +
+                "for\n" +
+                "from\n" +
+                "get\n" +
+                "give\n" +
+                "go\n" +
+                "have\n" +
+                "he\n" +
+                "her\n" +
+                "here\n" +
+                "him\n" +
+                "his\n" +
+                "how\n" +
+                "I\n" +
+                "if\n" +
+                "in\n" +
+                "into\n" +
+                "it\n" +
+                "its\n" +
+                "just\n" +
+                "know\n" +
+                "like\n" +
+                "look\n" +
+                "make\n" +
+                "man\n" +
+                "many\n" +
+                "me\n" +
+                "more\n" +
+                "my\n" +
+                "new\n" +
+                "no\n" +
+                "not\n" +
+                "now\n" +
+                "of\n" +
+                "on\n" +
+                "one\n" +
+                "only\n" +
+                "or\n" +
+                "other\n" +
+                "our\n" +
+                "out\n" +
+                "people\n" +
+                "say\n" +
+                "see\n" +
+                "she\n" +
+                "so\n" +
+                "some\n" +
+                "take\n" +
+                "tell\n" +
+                "than\n" +
+                "that\n" +
+                "the\n" +
+                "their\n" +
+                "them\n" +
+                "then\n" +
+                "there\n" +
+                "these\n" +
+                "they\n" +
+                "thing\n" +
+                "think\n" +
+                "this\n" +
+                "those\n" +
+                "time\n" +
+                "to\n" +
+                "two\n" +
+                "up\n" +
+                "use\n" +
+                "very\n" +
+                "want\n" +
+                "way\n" +
+                "we\n" +
+                "well\n" +
+                "what\n" +
+                "when\n" +
+                "which\n" +
+                "who\n" +
+                "will\n" +
+                "with\n" +
+                "would\n" +
+                "year\n" +
+                "you\n" +
+                "your\n";
+
+        String arr[] = shit.split("\n");
+        for(String s: arr)
+        {
+            System.out.print("\""+s+"\""+", ");
+        }
+    }
+}
